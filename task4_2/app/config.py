6f331108711dfa23dfd8d54a7fe1fd1066244a1c
AnandDeakin <139202909+AnandDeakin@users.noreply.github.com>
2024-08-26 00:39:38 +0800

    Add task4.2
    Add task4.2 and updated with external DB url in config.py file

---
+++ task4_2/app/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Settings:
+    DATABASE_URL: str = os.getenv("OutRaLcvrsLpFCkNsqhCurpW2ha9e0m4@dpg-cr5ldatumphs73e5msv0-a.oregon-postgres.render.com/library_database_bngk")
+
+settings = Settings()
