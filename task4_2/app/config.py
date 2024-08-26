b552934a2cfe9b87b0961599e83a05f029e1f3d3
Anand Bhupathi Medarametla <s223684301@deakin.edu.au>
2024-08-27 04:19:05 +1000

    changes reverted

--- task4_2/app/config.py
+++ task4_2/app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("OutRaLcvrsLpFCkNsqhCurpW2ha9e0m4@dpg-cr5ldatumphs73e5msv0-a.oregon-postgres.render.com/library_database_bngk")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
